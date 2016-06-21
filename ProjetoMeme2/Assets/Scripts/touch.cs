48913c2e7161479c737ca3ac54836b2f8537c784
alineelima <alinee.lima_@hotmail.com>
2016-06-21 15:01:56 -0400

    Eduardo nao gostou

---
+++ ProjetoMeme2/Assets/Scripts/touch.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class touch : MonoBehaviour {
+
+	public GameObject Barrinha,Barrinha2;
+	public float x;
+
+	public void Stop(){
+
+		if(Barrinha.transform.position.x >= -0.46 && Barrinha.transform.position.x <= 0.46){
+			x = x+0.5f;
+			Debug.Log ("Yay");
+			Barrinha2.transform.localScale += new Vector3(0,x,0);
+			Debug.Log (x);
+		}
+	}
+}
