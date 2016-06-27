316c58c52391c2fecb6522000fd0155a24d2e886
Jhone Mendonça <g.m.enohj@gmail.com>
2016-06-27 17:26:52 -0400

    Mute button

--- ProjetoMeme2/Assets/Scripts/MuteSound.cs
+++ ProjetoMeme2/Assets/Scripts/MuteSound.cs
@@ -3,14 +3,14 @@ using System.Collections;
 using UnityEngine.UI; 
 
 public class MuteSound : MonoBehaviour {
-	public string SoundButtonTAG;
+	//public string SoundButtonTAG;
 	public Sprite ButtonON, ButtonOFF;
 	private bool muted =false;
-	private GameObject SoundButton;
+	public  GameObject SoundButton;
 	//private Animator SounderAni;
 	// Use this for initialization
 	void Start () {
-		SoundButton= GameObject.FindGameObjectWithTag (SoundButtonTAG);
+		//SoundButton= GameObject.FindGameObjectWithTag (SoundButtonTAG);
 		//SounderAni = SoundButton.GetComponent<Animator> ();
 		if (AudioListener.volume == 0.0f) {
 			muted= true;
